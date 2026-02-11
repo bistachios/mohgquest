4afd1499ccaa6437eb66e37daadf2cd38d6b44f3
bistachios <vii.ljubim@gmail.com>
2026-02-11 15:31:34 -0500

    added npc conversation and quest_given flag

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ NPC_SCALING = 0.8
 SPRITE_NATIVE_SIZE = 128
 SPRITE_SIZE = int(SPRITE_NATIVE_SIZE * SPRITE_SCALING)
 
-TALK_DISTANCE = 70
+TALK_DISTANCE = 100
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -14,7 +14,7 @@ WINDOW_TITLE = "Mohg Quest"
 MOVEMENT_SPEED = 5
 
 BACKGROUND_1 = arcade.load_texture("assets/images/mohgwynpalacebg.png")
-BACKGROUND_2 = arcade.load_texture(":resources:images/backgrounds/abstract_2.jpg")
+BACKGROUND_2 = arcade.load_texture("assets/images/MPBG2.png")
 
 
 class Room:
@@ -77,10 +77,22 @@ class GameView(arcade.View):
             self.rooms[self.current_room].wall_list,
         )
 
+        self.dialogue_lines = [
+            "Dearest Ansbach...it seems we have a Tarnished visitor. Would you attend to them, please?",
+            "At once, Lord Mohg.",
+            "Thank you. Return to me when the deed is done."
+        ]
+        self.dialogue_index = 0
         self.show_dialogue = False
+        self.dialogue_text = ""
+
+        self.quest_given = False
+    
+    def display_dialogue(self, text):
+        self.dialogue_text = text
+        self.show_dialogue = True
 
     def on_draw(self):
-
         self.clear()
 
         arcade.draw_texture_rect(
@@ -94,11 +106,10 @@ class GameView(arcade.View):
         self.npc_list.draw()
 
         if self.show_dialogue:
-            arcade.draw_lrtb_rectangle_filled(0, WINDOW_WIDTH, 150, 0, arcade.color.BLACK)
-            arcade.draw_text(self.dialogue_text, 20, 100, arcade.color.WHITE, 16)
+            arcade.draw_lrbt_rectangle_filled(250, WINDOW_WIDTH, 0, 150, arcade.color.BLACK)
+            arcade.draw_text(self.dialogue_text, 270, 100, arcade.color.WHITE, 16)
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
         if key == arcade.key.LEFT:
             self.player_sprite.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
@@ -106,8 +117,19 @@ class GameView(arcade.View):
         elif key == arcade.key.E:
             distance = arcade.get_distance_between_sprites(self.player_sprite, self.npc_sprite)
             if distance < TALK_DISTANCE:
-                self.show_dialogue = True
-                self.display_dialogue("Dearest Ansbach...it seems we have a Tarnished visitor.")
+                if self.quest_given and not self.show_dialogue:
+                    self.display_dialogue("Back so soon? Did you wish for a kiss before you left, my dear?")
+                    return
+                if not self.show_dialogue:
+                    self.show_dialogue = True
+                    self.dialogue_index = 0
+                else:
+                    self.dialogue_index += 1
+                if self.dialogue_index < len(self.dialogue_lines):
+                    self.display_dialogue(self.dialogue_lines[self.dialogue_index])
+                else:
+                    self.show_dialogue = False
+                    self.quest_given = True
 
     def on_key_release(self, key, modifiers):
 
