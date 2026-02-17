d01597488c600db619af7afa1b9cca932cdb9828
bistachios <vii.ljubim@gmail.com>
2026-02-17 17:17:48 -0500

    changed sprite logic to be room-specific, put room class in separate module

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 import arcade
-
-SPRITE_SCALING = 0.5
-NPC_SCALING = 0.8
-SPRITE_NATIVE_SIZE = 128
-SPRITE_SIZE = int(SPRITE_NATIVE_SIZE * SPRITE_SCALING)
-
-TALK_DISTANCE = 150
-
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Mohg Quest"
-
-MOVEMENT_SPEED = 5
-
-BACKGROUND_1 = arcade.load_texture("assets/images/MPBG.png")
-BACKGROUND_2 = arcade.load_texture("assets/images/MPBG2.png")
-
-
-class Room:
-    def __init__(self, background):
-        self.wall_list = arcade.SpriteList()
-        self.background = background
-
-def setup_room_1():
-    room = Room(BACKGROUND_1)
-    return room
-
-def setup_room_2():
-    room = Room(BACKGROUND_2)
-    return room
+from constants import *
+from rooms import Room1, Room2
 
 class GameView(arcade.View):
     def __init__(self):
@@ -51,118 +23,36 @@ class GameView(arcade.View):
         self.player_list = arcade.SpriteList()
         self.player_list.append(self.player_sprite)
 
-        self.npc_sprite = arcade.Sprite(
-            "assets/images/mohg.PNG",
-            scale=NPC_SCALING
-        )
-
-        self.npc_sprite.center_x = 100
-        self.npc_sprite.center_y = 150
-        self.npc_list = arcade.SpriteList()
-        self.npc_list.append(self.npc_sprite)
 
         self.rooms = []
-
-        room = setup_room_1()
-        self.rooms.append(room)
-
-        room = setup_room_2()
-        self.rooms.append(room)
+        self.rooms.append(Room1())
+        self.rooms.append(Room2())
 
         self.current_room = 0
 
-        # Create a physics engine for this room
         self.physics_engine = arcade.PhysicsEngineSimple(
             self.player_sprite,
             self.rooms[self.current_room].wall_list,
         )
 
-        arcade.load_font("assets/fonts/Quintessential-Regular.ttf")
-
-        self.tutorial_text_object = arcade.Text(
-            text="Talk [E]",
-            x=50,
-            y= 300,
-            color=arcade.color.WHITE,
-            font_size=40,
-            font_name="Quintessential"
-        )
-
-        self.speaker_text_object = arcade.Text(
-            text="Mohg, Lord of Blood",
-            x=275,
-            y=160,
-            color=arcade.color.RED,
-            font_size=20,
-            font_name="Quintessential"
-        )
-
-        self.dialogue_text_object = arcade.Text(
-            text="",
-            x=290,
-            y=100,
-            color=arcade.color.WHITE,
-            font_size=20,
-            font_name="Quintessential",
-            multiline=True,
-            width=800
-        )
-
-        self.dialogue_lines = [
-            {"speaker": "Mohg, Lord of Blood", "text": "Dearest Ansbach...it seems we have a Tarnished visitor."},
-            {"speaker": "Mohg, Lord of Blood", "text": "They're disturbing the Albinaurics at the entrance to our domain.\nWould you attend to them, please?"},
-            {"speaker": "Pureblood Knight Ansbach", "text": "At once, Lord Mohg."},
-            {"speaker": "Mohg, Lord of Blood", "text": "Thank you. Return to me when the deed is done."}
-        ]
-        self.dialogue_index = 0
-        self.show_dialogue = False
-
         self.quest_given = False
-    
-    
-    def update_dialogue(self):
-        if self.dialogue_index < len(self.dialogue_lines):
-            current_line = self.dialogue_lines[self.dialogue_index]
-        
-            self.speaker_text_object.text = current_line["speaker"]
-            self.dialogue_text_object.text = current_line["text"]
-        
-            if current_line["speaker"] == "Pureblood Knight Ansbach":
-                self.speaker_text_object.color = arcade.color.SILVER
-            else:
-                self.speaker_text_object.color = arcade.color.RED
-            
-            self.show_dialogue = True
 
     def on_draw(self):
         self.clear()
+    
+        current_room = self.rooms[self.current_room]
 
         arcade.draw_texture_rect(
-            self.rooms[self.current_room].background,
-            rect=arcade.LBWH(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
+        current_room.background,
+        rect=arcade.LBWH(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
         )
 
-        self.rooms[self.current_room].wall_list.draw()
+        current_room.wall_list.draw()
+        current_room.room_sprites.draw()
 
         self.player_list.draw()
-        self.npc_list.draw()
-
-        distance = arcade.get_distance_between_sprites(self.player_sprite, self.npc_sprite)
-        if distance < TALK_DISTANCE:
-            self.tutorial_text_object.draw()
-
-        if self.show_dialogue:
-            arcade.draw_lrbt_rectangle_filled(250, 1100, 30, 200, arcade.color.BLACK)
-            arcade.draw_lrbt_rectangle_outline(
-    250, 
-    1100, 
-    30, 
-    200, 
-    arcade.color.GOLD, 
-    border_width=4
-)
-            self.dialogue_text_object.draw()
-            self.speaker_text_object.draw()
+        
+        current_room.draw_ui(self.player_sprite)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
@@ -172,24 +62,26 @@ class GameView(arcade.View):
             self.player_sprite.scale_x = SPRITE_SCALING
             self.player_sprite.change_x = MOVEMENT_SPEED
         elif key == arcade.key.E:
-            distance = arcade.get_distance_between_sprites(self.player_sprite, self.npc_sprite)
-            if distance < TALK_DISTANCE:
-                if self.quest_given and not self.show_dialogue:
-                    self.speaker_text_object.text = "Mohg, Lord of Blood"
-                    self.dialogue_text_object.text = "Back so soon? Did you wish for a kiss before you left, my dear?"
-                    self.show_dialogue = True
-                    return
-                if not self.show_dialogue:
-                    self.dialogue_index = 0
-                    self.show_dialogue = True
-                    self.update_dialogue()
-                else:
-                    self.dialogue_index += 1
-                    if self.dialogue_index < len(self.dialogue_lines):
-                        self.update_dialogue()
+            current_room = self.rooms[self.current_room]
+            if hasattr(current_room, "npc_sprite"):
+                distance = arcade.get_distance_between_sprites(self.player_sprite, current_room.npc_sprite)
+                if distance < TALK_DISTANCE:
+                    if self.quest_given and not current_room.show_dialogue:
+                        current_room.speaker_text_object.text = "Mohg, Lord of Blood"
+                        current_room.dialogue_text_object.text = "Back so soon? Did you wish for a kiss before you left, my dear?"
+                        current_room.show_dialogue = True
+                        return
+                    if not current_room.show_dialogue:
+                        current_room.dialogue_index = 0
+                        current_room.show_dialogue = True
+                        current_room.update_dialogue()
                     else:
-                        self.show_dialogue = False
-                        self.quest_given = True
+                        current_room.dialogue_index += 1
+                        if current_room.dialogue_index < len(current_room.dialogue_lines):
+                            current_room.update_dialogue()
+                        else:
+                            current_room.show_dialogue = False
+                            self.quest_given = True
 
     def on_key_release(self, key, modifiers):
 
---
+++ rooms.py
@@ -0,0 +1,116 @@
+import arcade
+from constants import *
+
+
+BACKGROUND_1 = arcade.load_texture("assets/images/MPBG.png")
+BACKGROUND_2 = arcade.load_texture("assets/images/MPBG2.png")
+
+class Room:
+    def __init__(self, background):
+        self.background = background
+        self.wall_list = arcade.SpriteList()
+        self.room_sprites = arcade.SpriteList()
+    
+    def draw_ui(self, player):
+        pass
+    
+class Room1(Room):
+    def __init__(self):
+        super().__init__(BACKGROUND_1)
+
+        self.npc_sprite = arcade.Sprite(
+            "assets/images/mohg.PNG",
+            scale=NPC_SCALING
+        )
+
+        self.npc_sprite.center_x = 100
+        self.npc_sprite.center_y = 150
+        self.room_sprites.append(self.npc_sprite)
+
+        arcade.load_font("assets/fonts/Quintessential-Regular.ttf")
+
+        self.tutorial_text_object = arcade.Text(
+            text="Talk [E]",
+            x=50,
+            y= 300,
+            color=arcade.color.WHITE,
+            font_size=40,
+            font_name="Quintessential"
+        )
+
+        self.speaker_text_object = arcade.Text(
+            text="Mohg, Lord of Blood",
+            x=275,
+            y=160,
+            color=arcade.color.RED,
+            font_size=20,
+            font_name="Quintessential"
+        )
+
+        self.dialogue_text_object = arcade.Text(
+            text="",
+            x=290,
+            y=100,
+            color=arcade.color.WHITE,
+            font_size=20,
+            font_name="Quintessential",
+            multiline=True,
+            width=800
+        )
+
+        self.dialogue_lines = [
+            {"speaker": "Mohg, Lord of Blood", "text": "Dearest Ansbach...it seems we have a Tarnished visitor."},
+            {"speaker": "Mohg, Lord of Blood", "text": "They're disturbing the Albinaurics at the entrance to our domain.\nWould you attend to them, please?"},
+            {"speaker": "Pureblood Knight Ansbach", "text": "At once, Lord Mohg."},
+            {"speaker": "Mohg, Lord of Blood", "text": "Thank you. Return to me when the deed is done."}
+        ]
+        self.dialogue_index = 0
+        self.show_dialogue = False
+
+    def update_dialogue(self):
+        if self.dialogue_index < len(self.dialogue_lines):
+            current_line = self.dialogue_lines[self.dialogue_index]
+        
+            self.speaker_text_object.text = current_line["speaker"]
+            self.dialogue_text_object.text = current_line["text"]
+        
+            if current_line["speaker"] == "Pureblood Knight Ansbach":
+                self.speaker_text_object.color = arcade.color.SILVER
+            else:
+                self.speaker_text_object.color = arcade.color.RED
+            
+            self.show_dialogue = True
+
+    def draw_ui(self, player):
+        distance = arcade.get_distance_between_sprites(player, self.npc_sprite)
+        if distance < TALK_DISTANCE:
+            self.tutorial_text_object.draw()
+
+        if self.show_dialogue:
+            arcade.draw_lrbt_rectangle_filled(250, 1100, 30, 200, arcade.color.BLACK)
+            arcade.draw_lrbt_rectangle_outline(
+            250, 
+            1100, 
+            30, 
+            200, 
+            arcade.color.GOLD, 
+            border_width=4
+        )
+            self.dialogue_text_object.draw()
+            self.speaker_text_object.draw()
+
+class Room2(Room):
+    def __init__(self):
+        super().__init__(BACKGROUND_2)
+
+        self.enemy_sprite = arcade.Sprite(
+            "assets/images/tarnished.png",
+            scale=SPRITE_SCALING,
+        )
+
+        self.enemy_sprite.center_x = 1050
+        self.enemy_sprite.center_y = 100
+        self.room_sprites.append(self.enemy_sprite)
+    
+    def draw_ui(self, player):
+        pass
